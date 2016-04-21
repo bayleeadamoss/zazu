1b9676cf0359226349338f9e55829161e882a324
Blaine Schmeisser <blainesch@gmail.com>
2016-04-21 00:07:08 -0700

    Update build script.

--- tasks/build/build.js
+++ tasks/build/build.js
@@ -1,30 +1,19 @@
 'use strict';
 
 var pathUtil = require('path');
-var Q = require('q');
 var gulp = require('gulp');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
 var batch = require('gulp-batch');
 var plumber = require('gulp-plumber');
 var jetpack = require('fs-jetpack');
+var babel = require('gulp-babel');
 
-var bundle = require('./bundle');
-var generateSpecImportsFile = require('./generate_spec_imports');
 var utils = require('../utils');
 
 var projectDir = jetpack;
 var srcDir = projectDir.cwd('./app');
 var destDir = projectDir.cwd('./build');
-
-var paths = {
-    copyFromAppDir: [
-        './node_modules/**',
-        './helpers/**',
-        './**/*.html',
-        './**/*.+(jpg|png|svg)'
-    ],
-};
 
 // -------------------------------------
 // Tasks
@@ -38,32 +27,22 @@ gulp.task('clean', function () {
 var copyTask = function () {
     return projectDir.copyAsync('app', destDir.path(), {
             overwrite: true,
-            matching: paths.copyFromAppDir
+            matching: [
+                'node_modules/**',
+                'data/**',
+                './**/*.+(html|jpg|png|svg)',
+            ]
         });
 };
 gulp.task('copy', ['clean'], copyTask);
 gulp.task('copy-watch', copyTask);
 
-
-var bundleApplication = function () {
-    return Q.all([
-            bundle(srcDir.path('background.js'), destDir.path('background.js')),
-            bundle(srcDir.path('app.js'), destDir.path('app.js')),
-        ]);
-};
-
-var bundleSpecs = function () {
-    return generateSpecImportsFile().then(function (specEntryPointPath) {
-        return bundle(specEntryPointPath, destDir.path('spec.js'));
-    });
-};
-
 var bundleTask = function () {
-    if (utils.getEnvName() === 'test') {
-        return bundleSpecs();
-    }
-    return bundleApplication();
+    return gulp.src(['app/**/*.js', '!app/{node_modules,node_modules/**}'])
+        .pipe(babel())
+        .pipe(gulp.dest('build'));
 };
+
 gulp.task('bundle', ['clean'], bundleTask);
 gulp.task('bundle-watch', bundleTask);
 
@@ -78,7 +57,7 @@ gulp.task('less', ['clean'], lessTask);
 gulp.task('less-watch', lessTask);
 
 
-gulp.task('finalize', ['clean'], function () {
+gulp.task('finalize', ['clean', 'copy'], function () {
     var manifest = srcDir.read('package.json', 'json');
 
     // Add "dev" or "test" suffix to name, so Electron will write all data
@@ -107,7 +86,7 @@ gulp.task('watch', function () {
     watch('app/**/*.js', batch(function (events, done) {
         gulp.start('bundle-watch', done);
     }));
-    watch(paths.copyFromAppDir, { cwd: 'app' }, batch(function (events, done) {
+    watch('./node_modules/**', { cwd: 'app' }, batch(function (events, done) {
         gulp.start('copy-watch', done);
     }));
     watch('app/**/*.less', batch(function (events, done) {
