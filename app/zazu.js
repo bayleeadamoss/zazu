7fc28c0aae3131aa31828430590884d4d376866d
Blaine Schmeisser <blainesch@gmail.com>
2016-04-21 20:47:05 -0700

    Incremental react commit.

--- app/zazu.js
+++ app/zazu.js
@@ -1,29 +1,67 @@
-import Plugin from './plugin'
 import Theme from './theme'
 import configuration from './configuration'
 
-export default class Zazu {
-  constructor () {
+import React from 'react'
+
+const Zazu = React.createClass({
+
+  getInitialState () {
+    return {
+      theme: '',
+      results: [],
+    }
+  },
+
+  componentDidMount () {
     configuration.load()
-    this.plugins = configuration.plugins.map((gitUrl) => {
-      const plugin = new Plugin(gitUrl, configuration.pluginDir)
-      plugin.load()
-      return plugin
-    })
-  }
 
-  loadTheme () {
     const theme = new Theme(configuration.theme, configuration.pluginDir)
-    return theme.load()
-  }
-
-  search (input, callback) {
-    let responses = []
-    this.plugins.forEach((plugin) => {
-      if (plugin.respondsTo(input)) {
-        responses = responses.concat(plugin.search(input))
-      }
+    theme.load().then((theme) => {
+      this.setState({
+        theme,
+      })
     })
-    callback(responses)
-  }
-}
+
+    setTimeout(() => {
+      this.setState({
+        results: [
+          { value: 'Blaine' },
+          { value: 'Jared' },
+          { value: 'Adam' },
+          { value: 'Micah' },
+        ],
+      })
+    }, 1000)
+  },
+
+  handleChange (event) {
+    const query = event.target.value
+    this.setState({
+      query,
+    })
+  },
+
+  render () {
+    return (
+      <div>
+        <style>
+          { this.state.theme.css }
+        </style>
+        <input
+          type='text'
+          onChange={this.handleChange}
+          value={this.state.query} />
+        <ul>
+          {this.state.results.map((item) => {
+            return (
+              <li onclick={() => { this.action(item) } }>{item.value}</li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  },
+
+})
+
+export default Zazu
