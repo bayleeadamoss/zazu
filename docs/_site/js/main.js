7d945e0fd196d84467c666fd9fb77d00aef8bb45
Blaine Schmeisser <blainesch@gmail.com>
2016-05-19 18:49:26 -0700

    Remove docs site.

--- docs/_site/js/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Finds all the headers that can link to themselves and make them clickable.
- */
-jQuery(document).ready(function() {
-  jQuery('.c-article__main.sidebar').find('h2, h3').filter('[id]').each(function() {
-    var el = $(this);
-    el.html('<a href="#' + el.attr('id') + '">' + el.text() + '</a>');
-  });
-});
