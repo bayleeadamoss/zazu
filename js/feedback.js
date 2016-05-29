4a822d1f15ed8f7dc293d00453f91a606c86a4b3
Blaine Schmeisser <blainesch@gmail.com>
2016-05-29 11:32:43 -0700

    Updates

--- js/feedback.js
+++ js/feedback.js
@@ -162,6 +162,9 @@ var Feedback = React.createClass({
     }
   },
   handleSubmit: function () {
+    this.setState({
+      completed: true,
+    })
   },
   render: function () {
     return this.state.completed ? (
