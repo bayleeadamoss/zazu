96061eeedbecf6f56e8962acafa2b2b024e7ead4
Blaine Schmeisser <blainesch@gmail.com>
2016-06-06 08:38:01 -0700

    Update the default zazu file to have more sensible defaults.

--- templates/zazurc.js
+++ templates/zazurc.js
@@ -1,5 +1,7 @@
 module.exports = {
   'hotkey': 'alt+space',
-  'theme': 'tinytacoteam/dark-theme',
-  'plugins': [],
+  'theme': 'tinytacoteam/light-theme',
+  'plugins': [
+    'tinytacoteam/calculator',
+  ],
 }
