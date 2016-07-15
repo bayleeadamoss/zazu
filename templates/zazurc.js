fea45024117292bf4602cbc1f9e359b8c3eb89d4
Blaine Schmeisser <blainesch@gmail.com>
2016-07-15 11:36:51 -0700

    Update default configuration.

--- templates/zazurc.js
+++ templates/zazurc.js
@@ -2,6 +2,13 @@ module.exports = {
   'hotkey': 'alt+space',
   'theme': 'tinytacoteam/zazu-light-theme',
   'plugins': [
+    'tinytacoteam/zazu-file-finder',
     'tinytacoteam/zazu-calculator',
+    {
+      name: 'tinytacoteam/zazu-clipboard',
+      variables: {
+        ClipboardKey: 'cmd+shift+v',
+      },
+    },
   ],
 }
