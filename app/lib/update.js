69ab0571bcf4975680c30672cee7a3bf5f0b740b
Blaine Schmeisser <blainesch@gmail.com>
2016-07-15 14:42:16 -0700

    Update the auto updater to just link to website.

--- app/lib/update.js
+++ app/lib/update.js
@@ -1,42 +1,69 @@
-const GhReleases = require('electron-gh-releases')
+const semver = require('semver')
+const https = require('https')
 const { shell, app, dialog } = require('electron')
+
+const globalEmitter = require('./globalEmitter')
 
 class Update {
 
   constructor () {
-    this.updater = new GhReleases({
-      repo: 'tinytacoteam/zazu',
-      currentVersion: app.getVersion(),
+    this._latestVersion = null
+  }
+
+  latestVersion () {
+    return new Promise((resolve, reject) => {
+      if (this._latestVersion) {
+        return resolve(this._latestVersion)
+      }
+      https.get({
+        host: 'api.github.com',
+        path: '/repos/tinytacoteam/zazu/releases/latest',
+        headers: {
+          'User-Agent': `ZazuApp Updater v${app.getVersion()}`,
+        },
+      }, (res) => {
+        var chunks = []
+        res.on('data', (chunk) => {
+          chunks.push(chunk.toString())
+        })
+        res.on('end', () => {
+          this._latestVersion = JSON.parse(chunks.join()).tag_name
+          resolve(this._latestVersion || app.getVersion())
+        })
+      }).on('error', (e) => {
+        reject(`Got error: ${e.message}`)
+      })
     })
-    this.updater.on('update-downloaded', (info) => {
-      this.updater.install()
+  }
+
+  needsUpdate () {
+    return this.latestVersion().then((newestVersion) => {
+      return semver.satisfies(newestVersion, `>${app.getVersion()}`) && newestVersion
     })
   }
 
   check (manualUpdate) {
-    this.updater.check((err, hasUpdate) => {
-      if (hasUpdate) {
+    this.needsUpdate().then((updateVersion) => {
+      if (updateVersion) {
+        globalEmitter.emit('showWindow')
         dialog.showMessageBox({
           type: 'question',
           buttons: ['Ignore', 'Download'],
           defaultId: 1,
           cancelId: 0,
-          title: 'Newer version available!',
-          message: 'A new Zazu is available for download!',
+          title: 'Zazu Updater',
+          message: 'Zazu v' + updateVersion + ' is available for download!',
           detail: 'Click download to get the newest version of Zazu!',
         }, (response) => {
           if (response === 1) {
-            if (err) {
-              shell.openExternal('http://zazuapp.org/') // linux
-            } else {
-              this.updater.download() // mac or windows
-            }
+            shell.openExternal('http://zazuapp.org/')
           }
         })
       } else if (manualUpdate) {
         dialog.showMessageBox({
           type: 'none',
           title: 'Zazu Updater',
+          message: 'Your Zazu version is up to date!',
           detail: 'No Zazu Updates were found.',
           buttons: [],
         })
