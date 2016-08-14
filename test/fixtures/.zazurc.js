1380525dd2442f663948bcdf35d177b6157c3ba1
Blaine Schmeisser <blainesch@gmail.com>
2016-08-14 15:28:52 -0700

    Add an additional stub for zazurc.js

--- test/fixtures/.zazurc.js
+++ test/fixtures/.zazurc.js
@@ -1,9 +1,10 @@
 // zazu-fixtures is installed
+// zazu-calculator is not
 module.exports = {
   hotkey: 'shift+space',
   theme: 'tinytacoteam/zazu-fixture',
   disableAnalytics: true,
   plugins: [
-    'tinytacoteam/zazu-fixture',
+    'tinytacoteam/zazu-calculator',
   ],
 }
