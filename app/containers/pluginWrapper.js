435c47b5cc0c744201c555d628ad5ec2df833aec
cecoates <charles.e.coates@gmail.com>
2017-01-12 22:26:10 -0600

    Update pluginWrapper.js

--- app/containers/pluginWrapper.js
+++ app/containers/pluginWrapper.js
@@ -129,16 +129,13 @@ const PluginWrapper = React.createClass({
     })
   },
 
-handleQueryChange (query) {
+  handleQueryChange (query) {
     let first = true
-    const interaction = track.interaction() 
+    const interaction = track.interaction()
     interaction.setName('search')
     interaction.setAttribute('queryLength', query.length)
     this.context.logger.log('info', `Updating query to "${query}"`)
-    handleQuerySearch={this.handleQuerySearch}
-    }
 
-handleQuerySearch (query) {
     const promises = this.state.plugins.filter((plugin) => {
       return plugin.respondsTo(query)
     }).reduce((memo, plugin) => {
@@ -179,7 +176,6 @@ handleQuerySearch (query) {
       query,
     })
   },
-
 
   handleResultClick (result) {
     this.context.logger.log('info', 'actioned result', result)
