43d0f76ffc4959123515a0a3db26b9231d76e55a
Leonid Kukuyev <leonidkukuyev@gmail.com>
2017-02-06 22:54:42 -0500

    fix onClick antipattern, change to component class

--- app/components/noplugins.js
+++ app/components/noplugins.js
@@ -2,9 +2,8 @@ const React = require('react')
 const Style = require('./style')
 const {shell} = require('electron')
 
-
-const NoPlugins = () => {
-  let css = `
+const NoPlugins = React.createClass({
+  css: `
     body {
       background-color: #5CC7B2;
       margin: 0;
@@ -19,15 +18,21 @@ const NoPlugins = () => {
     a {
       color: #EBEBEB;
     }
-  `
+  `,
 
-  return (
-    <div className='container'>
-      <h2 className='loader'>Zazu requires plugins to work!</h2>
-      <h3 className='loader'>Have a look at the <a href='#' onClick={ () => { shell.openExternal('http://zazuapp.org/plugins/') } }>Zazu Plugins</a> page.</h3>
-      <Style css={css}/>
-    </div>
-  )
-}
+  handleLinkClick () {
+    shell.openExternal('http://zazuapp.org/plugins/')
+  },
+
+  render () {
+    return (
+      <div className='container'>
+        <h2 className='loader'>Zazu requires plugins to work!</h2>
+        <h3 className='loader'>Have a look at the <a href='#' onClick={this.handleLinkClick}>Zazu Plugins</a> page.</h3>
+        <Style css={this.css}/>
+      </div>
+    )
+  },
+})
 
 module.exports = NoPlugins
