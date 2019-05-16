80ff25578c6296b196177b835b9d5a29f9a1347e
linonetwo <linonetwo012@gmail.com>
2019-05-16 09:29:57 +0800

    chore: upgrade eslint-babel and add html plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": ["standard", "plugin:react/recommended"],
-  "rules": { "comma-dangle": [2, "always-multiline"] },
-  "plugins": [ "react" ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true,
+  parser: 'babel-eslint',
+  extends: ['standard', 'plugin:react/recommended'],
+  rules: { 'comma-dangle': [2, 'always-multiline'] },
+  plugins: ['react', 'html'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-  "settings": {
-    "react": {
-      "version": "16.8.6",
+  settings: {
+    react: {
+      version: '16.8.6',
     },
   },
-  "globals": {
-    "newrelic": true,
-    "__nr_require": true,
+  globals: {
+    newrelic: true,
+    __nr_require: true,
   },
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true,
-    "mocha": true,
-    "node": true,
-  }
-};
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true,
+    node: true,
+  },
+}
