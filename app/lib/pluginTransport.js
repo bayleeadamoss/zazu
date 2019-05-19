a3194448b0d857176209a0b3078c22d45ea2cb30
linonetwo <linonetwo012@gmail.com>
2019-05-19 15:37:05 +0800

    refactor: use new winston-transport

--- app/lib/pluginTransport.js
+++ app/lib/pluginTransport.js
@@ -1,44 +1,38 @@
+const Transport = require('winston-transport')
 const globalEmitter = require('./globalEmitter')
-const util = require('util')
-const winston = require('winston')
 
-var PluginLogger = (winston.transports.PluginLogger = function (options) {
-  this.name = 'pluginLogger'
-  this.level = options.level || 'verbose'
-  this.devtoolOpened = false
-  this.mainWindowOpened = false
-  globalEmitter.on('debuggerOpened', () => {
-    this.devtoolOpened = true
-  })
-  globalEmitter.on('debuggerClosed', () => {
+class PluginLogger extends Transport {
+  constructor (options) {
+    super(options)
+    this.name = 'pluginLogger'
+    this.level = options.level || 'verbose'
     this.devtoolOpened = false
-  })
-  globalEmitter.on('showWindow', () => {
-    this.mainWindowOpened = true
-  })
-  globalEmitter.on('hideWindow', () => {
     this.mainWindowOpened = false
-  })
-})
-
-util.inherits(PluginLogger, winston.Transport)
-
-PluginLogger.prototype.log = function (level, message, meta, callback) {
-  // only log plugin error when normal user won't be affected
-  if ((this.devtoolOpened === true || this.mainWindowOpened === false) && meta.plugin) {
-    globalEmitter.emit(
-      'pluginLog',
-      Object.assign(
-        {},
-        {
-          level,
-          message,
-        },
-        meta
-      )
-    )
+    globalEmitter.on('debuggerOpened', () => {
+      this.devtoolOpened = true
+    })
+    globalEmitter.on('debuggerClosed', () => {
+      this.devtoolOpened = false
+    })
+    globalEmitter.on('showWindow', () => {
+      this.mainWindowOpened = true
+    })
+    globalEmitter.on('hideWindow', () => {
+      this.mainWindowOpened = false
+    })
   }
-  callback(null, true)
+
+  log (level, message, meta, callback) {
+    // only log plugin error when normal user won't be affected
+    if ((this.devtoolOpened === true || this.mainWindowOpened === false) && meta.plugin) {
+      globalEmitter.emit('pluginLog', {
+        level,
+        message,
+        ...meta,
+      })
+    }
+    callback(null, true)
+  }
 }
 
 module.exports = PluginLogger
