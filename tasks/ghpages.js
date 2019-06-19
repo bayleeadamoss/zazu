e802f13aa414327e061f58eae00322992c404089
linonetwo <linonetwo012@gmail.com>
2019-06-19 15:09:14 +0800

    doc: update publish script for current file structure

--- tasks/ghpages.js
+++ tasks/ghpages.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 const { execSync } = require('child_process')
 
 const appVersion = () => {
-  const packageJson = path.join('app', 'package.json')
+  const packageJson = 'package.json'
   const packageContent = JSON.parse(fs.readFileSync(packageJson))
   return packageContent.version
 }
@@ -16,25 +16,41 @@ const commitSha = () => {
   return execSync('git rev-parse HEAD').toString()
 }
 
-const setupDynamicConfig = (docsConfigFile) => {
+const setupDynamicConfig = docsConfigFile => {
   fs.writeFileSync(docsConfigFile, `currentCommitSha: ${commitSha()}\n`, { flag: 'a' })
   fs.writeFileSync(docsConfigFile, `currentAppVersion: ${appVersion()}\n`, { flag: 'a' })
 }
 
-const cleanupDynamicConfig = (docsConfigFile) => {
+const cleanupDynamicConfig = docsConfigFile => {
   const data = fs.readFileSync(docsConfigFile)
-  fs.writeFileSync(docsConfigFile, data.toString().split('\n').filter((line) => {
-    return !line.match(/currentCommitSha|currentAppVersion/)
-  }).join('\n').trim() + '\n')
+  fs.writeFileSync(
+    docsConfigFile,
+    data
+      .toString()
+      .split('\n')
+      .filter(line => {
+        return !line.match(/currentCommitSha|currentAppVersion/)
+      })
+      .join('\n')
+      .trim() + '\n'
+  )
 }
 
 gulp.task('ghpages', () => {
   const docsDirectory = path.join('docs')
   const docsConfigFile = path.join(docsDirectory, '_config.yml')
   setupDynamicConfig(docsConfigFile)
-  ghpages.publish(docsDirectory, { dotfiles: true }, (err) => {
-    console.log('err', err)
-    cleanupDynamicConfig(docsConfigFile)
-    console.log('Deployed documentation')
+  console.log('start publishing')
+  return new Promise((resolve, reject) => {
+    ghpages.publish(docsDirectory, { dotfiles: true }, error => {
+      if (error) {
+        console.error(error)
+        reject(error)
+      } else {
+        cleanupDynamicConfig(docsConfigFile)
+        console.log('Deployed documentation')
+        resolve()
+      }
+    })
   })
 })
